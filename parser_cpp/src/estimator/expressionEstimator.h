8e2f8905689d6c59d7541b4bb12f6102d1908b9c
aleksey slovesnov <slovesnov@yandex.ru>
2026-09-13 20:53:02 +0300

    split projects

--- parser_cpp/src/estimator/expressionEstimator.h
+++ parser_cpp/src/estimator/expressionEstimator.h
@@ -57,7 +57,7 @@ class ExpressionEstimator {
 
 public:
 	//version for calculator/graph
-	static constexpr double version = 1.28; //format("%.2lf",)
+	static constexpr std::string version = "1.2.9";
 
 #ifdef EXPRESSION_ESTIMATOR_COUNT_NODES
 	static int totalCreated;
